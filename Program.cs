c2715b0f2ceee19f0dc4859489bc8d9a209a7d3a
vilmar237 <djilovilmar@gmail.com>
2024-11-12 17:52:27 -0500

    labo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace labo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub! Je m'appelle Vilmar DJILO");
+        }
+    }
+}
